959a9c4f83e452846f69cb4834a97bc9fb9ffb25
Kapil Dalwani <kapildalwani@gmail.com>
2026-02-03 16:27:50 -0800

    Linear regression and logistic regression

--- src/k_means_cluster.py
+++ src/k_means_cluster.py
@@ -30,10 +30,3 @@ if __name__ == "__main__":
     kmeans = Kmeans(k=5)
     X_data = np.random.randn(N, D)
     kmeans.fit(X_data)
-
-##  Two pointers
-
-| K-means | link                                                                                                       |
-|:-----|:--------------------------------------------------------------------------------------------------------------|
-| ✅  | [ML Interview k-means](https://github.com/alirezadir/Machine-Learning-Interviews/blob/main/src/MLC/notebooks/k_means.ipynb)             |
-|  ⬜    | [Implementing K-means Clustering from Scratch - in Python](https://mmuratarat.github.io/2019-07-23/kmeans_from_scratch) |
